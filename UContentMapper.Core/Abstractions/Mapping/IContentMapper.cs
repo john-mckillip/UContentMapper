fa65720b358364f796d4821dad12114aee801ea8
john-mckillip <john0mckillip@gmail.com>
2025-05-19 08:23:04 -0500

    Working on breaking out the main abstractions for this library.

---
+++ UContentMapper.Core/Abstractions/Mapping/IContentMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace UContentMapper.Core.Abstractions.Mapping
+{
+    public interface IContentMapper<TModel> where TModel : class
+    {
+        TModel Map(object source);
+        bool CanMap(object source);
+    }
+}
