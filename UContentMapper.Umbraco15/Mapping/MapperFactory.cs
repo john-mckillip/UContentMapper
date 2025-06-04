6064663f5d5b2f0e19dfcb978f680e6c84ef13aa
john-mckillip <john0mckillip@gmail.com>
2025-06-04 16:04:48 -0500

    Finished-up initial Umbraco 15 implementation of UContentMapper.

---
+++ UContentMapper.Umbraco15/Mapping/MapperFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UContentMapper.Core.Abstractions.Mapping;
+
+namespace UContentMapper.Umbraco15.Mapping
+{
+    /// <summary>
+    /// Factory for creating content mappers
+    /// </summary>
+    public class MapperFactory(IServiceProvider serviceProvider)
+    {
+        private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+        /// <summary>
+        /// Creates a content mapper for the specified model type
+        /// </summary>
+        public IContentMapper<TModel> CreateMapper<TModel>() where TModel : class
+        {
+            return _serviceProvider.GetRequiredService<IContentMapper<TModel>>();
+        }
+
+        /// <summary>
+        /// Creates a content mapper for the specified model type
+        /// </summary>
+        public IContentMapper<object> CreateMapperForType(Type modelType)
+        {
+            var mapperType = typeof(IContentMapper<>).MakeGenericType(modelType);
+            return (IContentMapper<object>)_serviceProvider.GetRequiredService(mapperType);
+        }
+    }
+}
